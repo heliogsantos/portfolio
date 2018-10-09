b9e97c91228f4485b867116ac74808655a254c3f
ringote <helio.santosjunior@outlook.com>
2018-10-08 22:59:21 -0300

    Criei Box jobs e adicionei imagens nas boxs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-// My Script
+$(function(){
+	var scroll = $(".scroll");
+	var corpo = $('html,body');
+
+	$(document).ready(function($) { 
+    scroll.click(function(event){        
+        event.preventDefault();
+        corpo.animate({scrollTop:$(this.hash).offset().top -40.}, 1100);
+  	 });
+   });
+}());

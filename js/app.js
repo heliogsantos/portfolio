f4bc58309d03c274f30c7c5d5d7173c6c1f8c8c7
ringote <helio.santosjunior@outlook.com>
2018-10-14 00:15:32 -0300

    Inserir Section E-mail Marketing e alterei a imagem sobre

--- js/app.js
+++ js/app.js
@@ -9,3 +9,37 @@ $(function(){
   	 });
    });
 }());
+
+// // Detalhes
+
+// function cont() {
+// myLoop();
+// myLoop2();   
+
+// var i = 0;                    
+// var totalCount = 160;
+// function myLoop () {           
+//    setTimeout(function () {    
+//       $('#contador2').html(i);          
+//       i++;                     
+//       if (i <= totalCount) {           
+//          myLoop();             
+//       }
+//    }, 10)
+// }
+// var c = 0;                    
+// var totalCount2 = 16;
+// function myLoop2 () {           
+//    setTimeout(function () {    
+//       $('#contador').html(c);          
+//       c++;                     
+//       if (c <= totalCount2) {           
+//          myLoop2();             
+//       }
+//     }, 100)
+//   }
+// }
+// cont();
+
+
+
